94d8d61f87dc80cf88789f77f7e554ecf24f868d
Rob Ward <robward@microsoft.com>
2017-11-21 15:09:12 -0800

    remove ODataBatchResponseItemTest

--- test/UnitTest/Microsoft.Test.AspNet.OData/Batch/ODataBatchResponseItemTest.cs
+++ test/UnitTest/Microsoft.Test.AspNet.OData/Batch/ODataBatchResponseItemTest.cs
@@ -17,86 +17,86 @@ namespace Microsoft.Test.AspNet.OData.Batch
 {
     public class ODataBatchResponseItemTest
     {
-        [Fact]
-        public void WriteMessageAsync_NullWriter_Throws()
-        {
-            ExceptionAssert.ThrowsArgumentNull(
-                () => ODataBatchResponseItem.WriteMessageAsync(null, new HttpResponseMessage(), CancellationToken.None)
-                    .Wait(),
-                "writer");
-        }
+    //    [Fact]
+    //    public void WriteMessageAsync_NullWriter_Throws()
+    //    {
+    //        ExceptionAssert.ThrowsArgumentNull(
+    //            () => ODataBatchResponseItem.WriteMessageAsync(null, new HttpResponseMessage(), CancellationToken.None)
+    //                .Wait(),
+    //            "writer");
+    //    }
 
-        [Fact]
-        public void WriteMessageAsync_NullResponse_Throws()
-        {
-            HttpContent content = new StringContent(String.Empty, Encoding.UTF8, "multipart/mixed");
-            content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("boundary", Guid.NewGuid().ToString()));
-            IODataResponseMessage odataResponse = ODataMessageWrapperHelper.Create(new MemoryStream(), content.Headers);
-            ODataMessageWriter messageWriter = new ODataMessageWriter(odataResponse);
+    //    [Fact]
+    //    public void WriteMessageAsync_NullResponse_Throws()
+    //    {
+    //        HttpContent content = new StringContent(String.Empty, Encoding.UTF8, "multipart/mixed");
+    //        content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("boundary", Guid.NewGuid().ToString()));
+    //        IODataResponseMessage odataResponse = ODataMessageWrapperHelper.Create(new MemoryStream(), content.Headers);
+    //        ODataMessageWriter messageWriter = new ODataMessageWriter(odataResponse);
 
-            ExceptionAssert.ThrowsArgumentNull(
-                () => ODataBatchResponseItem.WriteMessageAsync(messageWriter.CreateODataBatchWriter(), null, CancellationToken.None)
-                    .Wait(),
-                "response");
-        }
+    //        ExceptionAssert.ThrowsArgumentNull(
+    //            () => ODataBatchResponseItem.WriteMessageAsync(messageWriter.CreateODataBatchWriter(), null, CancellationToken.None)
+    //                .Wait(),
+    //            "response");
+    //    }
 
-        [Fact]
-        public void WriteMessageAsync_WritesResponseMessage()
-        {
-            MemoryStream ms = new MemoryStream();
-            HttpContent content = new StringContent(String.Empty, Encoding.UTF8, "multipart/mixed");
-            content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("boundary", Guid.NewGuid().ToString()));
-            IODataResponseMessage odataResponse = ODataMessageWrapperHelper.Create(ms, content.Headers);
-            var batchWriter = new ODataMessageWriter(odataResponse).CreateODataBatchWriter();
-            HttpResponseMessage response = new HttpResponseMessage()
-            {
-                Content = new StringContent("example content", Encoding.UTF8, "text/example")
-            };
-            response.Headers.Add("customHeader", "bar");
+    //    [Fact]
+    //    public void WriteMessageAsync_WritesResponseMessage()
+    //    {
+    //        MemoryStream ms = new MemoryStream();
+    //        HttpContent content = new StringContent(String.Empty, Encoding.UTF8, "multipart/mixed");
+    //        content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("boundary", Guid.NewGuid().ToString()));
+    //        IODataResponseMessage odataResponse = ODataMessageWrapperHelper.Create(ms, content.Headers);
+    //        var batchWriter = new ODataMessageWriter(odataResponse).CreateODataBatchWriter();
+    //        HttpResponseMessage response = new HttpResponseMessage()
+    //        {
+    //            Content = new StringContent("example content", Encoding.UTF8, "text/example")
+    //        };
+    //        response.Headers.Add("customHeader", "bar");
 
-            batchWriter.WriteStartBatch();
-            ODataBatchResponseItem.WriteMessageAsync(batchWriter, response, CancellationToken.None).Wait();
-            batchWriter.WriteEndBatch();
+    //        batchWriter.WriteStartBatch();
+    //        ODataBatchResponseItem.WriteMessageAsync(batchWriter, response, CancellationToken.None).Wait();
+    //        batchWriter.WriteEndBatch();
 
-            ms.Position = 0;
-            string result = new StreamReader(ms).ReadToEnd();
+    //        ms.Position = 0;
+    //        string result = new StreamReader(ms).ReadToEnd();
 
-            Assert.Contains("example content", result);
-            Assert.Contains("text/example", result);
-            Assert.Contains("customHeader", result);
-            Assert.Contains("bar", result);
-        }
+    //        Assert.Contains("example content", result);
+    //        Assert.Contains("text/example", result);
+    //        Assert.Contains("customHeader", result);
+    //        Assert.Contains("bar", result);
+    //    }
 
-        [Fact]
-        public void WriteMessageAsync_ResponseContainsContentId_IfHasContentIdInRequestChangeSet()
-        {
-            MemoryStream ms = new MemoryStream();
-            HttpContent content = new StringContent(String.Empty, Encoding.UTF8, "multipart/mixed");
-            content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("boundary", Guid.NewGuid().ToString()));
-            IODataResponseMessage odataResponse = ODataMessageWrapperHelper.Create(ms, content.Headers);
-            var batchWriter = new ODataMessageWriter(odataResponse).CreateODataBatchWriter();
-            HttpResponseMessage response = new HttpResponseMessage
-            {
-                Content = new StringContent("any", Encoding.UTF8, "text/example")
-            };
-            var request = new HttpRequestMessage();
-            var contentId = Guid.NewGuid().ToString();
-            request.SetODataContentId(contentId);
-            response.RequestMessage = request;
+    //    [Fact]
+    //    public void WriteMessageAsync_ResponseContainsContentId_IfHasContentIdInRequestChangeSet()
+    //    {
+    //        MemoryStream ms = new MemoryStream();
+    //        HttpContent content = new StringContent(String.Empty, Encoding.UTF8, "multipart/mixed");
+    //        content.Headers.ContentType.Parameters.Add(new NameValueHeaderValue("boundary", Guid.NewGuid().ToString()));
+    //        IODataResponseMessage odataResponse = ODataMessageWrapperHelper.Create(ms, content.Headers);
+    //        var batchWriter = new ODataMessageWriter(odataResponse).CreateODataBatchWriter();
+    //        HttpResponseMessage response = new HttpResponseMessage
+    //        {
+    //            Content = new StringContent("any", Encoding.UTF8, "text/example")
+    //        };
+    //        var request = new HttpRequestMessage();
+    //        var contentId = Guid.NewGuid().ToString();
+    //        request.SetODataContentId(contentId);
+    //        response.RequestMessage = request;
 
-            batchWriter.WriteStartBatch();
-            batchWriter.WriteStartChangeset();
-            ODataBatchResponseItem.WriteMessageAsync(batchWriter, response, CancellationToken.None).Wait();
-            batchWriter.WriteEndChangeset();
-            batchWriter.WriteEndBatch();
+    //        batchWriter.WriteStartBatch();
+    //        batchWriter.WriteStartChangeset();
+    //        ODataBatchResponseItem.WriteMessageAsync(batchWriter, response, CancellationToken.None).Wait();
+    //        batchWriter.WriteEndChangeset();
+    //        batchWriter.WriteEndBatch();
 
-            ms.Position = 0;
-            string result = new StreamReader(ms).ReadToEnd();
+    //        ms.Position = 0;
+    //        string result = new StreamReader(ms).ReadToEnd();
 
-            Assert.Contains("any", result);
-            Assert.Contains("text/example", result);
-            Assert.Contains("Content-ID", result);
-            Assert.Contains(contentId, result);
-        }
+    //        Assert.Contains("any", result);
+    //        Assert.Contains("text/example", result);
+    //        Assert.Contains("Content-ID", result);
+    //        Assert.Contains(contentId, result);
+    //    }
     }
 }
