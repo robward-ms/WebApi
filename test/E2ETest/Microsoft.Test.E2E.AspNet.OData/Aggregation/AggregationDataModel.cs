0ba1ddca056555a1aef556e6493c57b29dcef2ed
Rob Ward <robward@microsoft.com>
2018-02-26 15:47:10 -0800

    Add EFCore E2E project.

--- test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationDataModel.cs
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationDataModel.cs
@@ -1,8 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+#if EFCORE
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+#else
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+#endif
 
 namespace Microsoft.Test.E2E.AspNet.OData.Aggregation
 {
@@ -11,10 +16,17 @@ namespace Microsoft.Test.E2E.AspNet.OData.Aggregation
         public static string ConnectionString =
             @"Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=True;Persist Security Info=True;Database=AggregationTest";
 
+#if EFCORE
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=True;Database=AggregationTest-EFCore");
+        }
+#else
         public AggregationContext()
             : base(ConnectionString)
         {
         }
+#endif
 
         public DbSet<Customer> Customers { get; set; }
     }
--- test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationEdmModel.cs
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationEdmModel.cs
@@ -1,6 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+#if EFCORE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNet.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.Test.E2E.AspNet.OData.Common.Execution;
+#else
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -9,6 +19,7 @@ using System.Reflection;
 using Microsoft.AspNet.OData;
 using Microsoft.OData.Edm;
 using Microsoft.Test.E2E.AspNet.OData.Common.Execution;
+#endif
 
 namespace Microsoft.Test.E2E.AspNet.OData.Aggregation
 {
